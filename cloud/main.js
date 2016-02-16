4aec3bc31fb81773d47057380a82238a89a64daf
emnewxt <e.millet@newxt.fr>
2016-02-16 14:23:29 +0100

    Custom Cloud Code

--- cloud/main.js
+++ cloud/main.js
@@ -1,104 +1,198 @@
 var Parse = require('parse/node').Parse;
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hello world!');
-});
-
-Parse.Cloud.beforeSave('BeforeSaveFail', function(req, res) {
-  res.error('You shall not pass!');
-});
-
-Parse.Cloud.beforeSave('BeforeSaveFailWithPromise', function (req, res) {
-  var query = new Parse.Query('Yolo');
-  query.find().then(() => {
-   res.error('Nope');
-  }, () => {
-    res.success();
+function saveUpdatedAt() 
+{
+ // connectAsAdmin(function()
+ // {
+    var query = new Parse.Query(Parse.Object.extend("GlobalUpdatedAt"));
+    query.ascending("updatedAt").find(
+      {
+        success: function (results) { destroyGlobalUpdatedAtObjects(results, function() { createGlobalUpdatedAtObject()});},
+        error: function (error) { createGlobalUpdatedAtObject();}
   });
-});
+  //});
+};
 
-Parse.Cloud.beforeSave('BeforeSaveUnchanged', function(req, res) {
-  res.success();
-});
 
-Parse.Cloud.beforeSave('BeforeSaveChanged', function(req, res) {
-  req.object.set('foo', 'baz');
-  res.success();
-});
-
-Parse.Cloud.afterSave('AfterSaveTest', function(req) {
-  var obj = new Parse.Object('AfterSaveProof');
-  obj.set('proof', req.object.id);
-  obj.save();
-});
-
-Parse.Cloud.beforeDelete('BeforeDeleteFail', function(req, res) {
-  res.error('Nope');
-});
-
-Parse.Cloud.beforeSave('BeforeDeleteFailWithPromise', function (req, res) {
-  var query = new Parse.Query('Yolo');
-  query.find().then(() => {
-    res.error('Nope');
-  }, () => {
-    res.success();
-  });
-});
-
-Parse.Cloud.beforeDelete('BeforeDeleteTest', function(req, res) {
-  res.success();
-});
-
-Parse.Cloud.afterDelete('AfterDeleteTest', function(req) {
-  var obj = new Parse.Object('AfterDeleteProof');
-  obj.set('proof', req.object.id);
-  obj.save();
-});
-
-Parse.Cloud.beforeSave('SaveTriggerUser', function(req, res) {
-  if (req.user && req.user.id) {
-    res.success();
-  } else {
-    res.error('No user present on request object for beforeSave.');
+function destroyGlobalUpdatedAtObjects(objects, callback)
+{
+  if (objects && objects.length)
+  {
+    var taskCount = objects.length;
+    objects.forEach(function(object) 
+    {
+        object.destroy({
+        success: function (object) {taskCount--; if (taskCount < 1) {callback();} },
+        error: function (myObject, error) {taskCount--; if (taskCount < 1) {callback();} }
+      });
+    });
   }
-});
+  else {callback();}
+}
 
-Parse.Cloud.afterSave('SaveTriggerUser', function(req) {
-  if (!req.user || !req.user.id) {
-    console.log('No user present on request object for afterSave.');
-  }
-});
 
-Parse.Cloud.define('foo', function(req, res) {
-  res.success({
-    object: {
-      __type: 'Object',
-      className: 'Foo',
-      objectId: '123',
-      x: 2,
-      relation: {
-        __type: 'Object',
-        className: 'Bar',
-        objectId: '234',
-        x: 3
-      }
-    },
-    array: [{
-      __type: 'Object',
-      className: 'Bar',
-      objectId: '345',
-      x: 2
-    }],
-    a: 2
+function createGlobalUpdatedAtObject()
+{
+  var GlobalUpdatedAtClass = Parse.Object.extend("GlobalUpdatedAt");
+  var globalUpdatedAtObject = new GlobalUpdatedAtClass()
+  globalUpdatedAtObject.set("globalUpdatedAt", new Date())
+  globalUpdatedAtObject.save(null, 
+  {
+  success: function(object) { alert('New object created with objectId: ' + object.id);},
+  error: function(object, error) { alert('Failed to create new object, with error code: ' + error.message);}
   });
+}
+
+
+Parse.Cloud.afterSave("Categories", function(request) 
+{
+  saveUpdatedAt();
 });
 
-Parse.Cloud.define('bar', function(req, res) {
-  res.error('baz');
+Parse.Cloud.afterDelete("Categories", function(request)
+{
+  saveUpdatedAt();
 });
 
-Parse.Cloud.define('requiredParameterCheck', function(req, res) {
-  res.success();
-}, function(params) {
-  return params.name;
+
+Parse.Cloud.afterSave("CategoriesCountry", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("CategoriesCountry", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Countries", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Countries", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("CountriesFacecharts", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("CountriesFacecharts", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("CountriesVideos", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("CountriesVideos", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("FacechartProducts", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("FacechartProducts", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Facecharts", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Facecharts", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Faceparts", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Faceparts", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Languages", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Languages", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Products", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Products", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Status", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Status", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Translations", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Translations", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("Videos", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("Videos", function(request)
+{
+  saveUpdatedAt();
+});
+
+
+Parse.Cloud.afterSave("VideosCategories", function(request)
+{
+  saveUpdatedAt();
+});
+
+Parse.Cloud.afterDelete("VideosCategories", function(request)
+{
+  saveUpdatedAt();
 });
